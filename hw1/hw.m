e36b6399d9381579e5f33546da8e6444171d2e9f
Sr Kanna <kannas@flip3.engr.oregonstate.edu>
2017-04-13 19:02:45 -0700

    hw1&hw2

--- hw1/hw.m
+++ hw1/hw.m
@@ -23,13 +23,21 @@ X(:,14) = [];
 if (w2 == 1)	%weights
 	weight = ones(433,1);
 	Z = [weight,X];
+
+%for number five - adding random uniform weights
+	%---add the extra features here--%
 	W = ((transpose(Z)*Z)^-1)*transpose(Z)*Y;
-%	disp(W);
 	
 	if (tt == 1)
 		%training
+		
+		str = 'SSE USING WEIGHTS OF TRAINING DATA IS ';
+		disp(str);
 		SSE = transpose(Y-(Z*W))*(Y-Z*W);
 		disp(SSE);
+
+
+
 	else	%test------------------------------------------------------
 		filename = 'housing_test.txt';
 		[X,delimiterOut] = importdata(filename);
@@ -37,19 +45,31 @@ if (w2 == 1)	%weights
 		Y = X(:,14);
 		X(:,14) = [];
 
-%		Z = [weight,X];
-
+		Z = [weight,X];
+		str = 'SSE USING WEIGHTS OF TEST DATA IS ';
+		disp(str);
 		SSE = transpose(Y-(Z*W))*(Y-Z*W);
 		disp(SSE);
 	end
 
 else	%no weights
-	W = ((transpose(X)*X)^-1)*transpose(X)*Y;
+
+	l = .5;
+	I = transpose(X)*X;
+	%finding W for part six
+	W = (inv((transpose(X)*X)+l*I)*transpose(X)*Y);
+
+
+	%W = ((transpose(X)*X)^-1)*transpose(X)*Y;
 	disp(W);
 	if (tt == 1) %train
+		str = 'NO WEIGHTS AND TRAIN';
+		disp (str);
 		SSE = transpose(Y-(X*W))*(Y-X*W);
 		disp(SSE);
 	else
+		str = 'NO WEIGHTS AND TEST';
+		disp(str);
 		filename = 'housing_test.txt';
 		[X,delimiterOut] = importdata(filename);
 
