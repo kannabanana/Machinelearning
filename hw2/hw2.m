3f09c723d320f6f1ee35899935a8fa7d1d56fc2e
Sr Kanna <kannas@flip3.engr.oregonstate.edu>
2017-04-18 11:42:17 -0700

    learning rates

--- hw2/hw2.m
+++ hw2/hw2.m
@@ -6,16 +6,20 @@ filename = 'usps-4-9-train.csv';
 y = x(:,end);
 x(:,end) = [];
 
-learning_rate = .0001;
+learning_rate = [.2,.02,.002,.0002,.0002,.00002,.000002,.000002,.00000002];
 
 
-w = zeros(1,256);
-for i = 1:1500
-	delta = zeros(1,256);
-	for j=1:1400
-		y1 = 1/(1+(exp(-(w)*(transpose(x(j,:))))));
-		err = y(j)-y1;
-		delta = delta+(err*x(j,:));
+for z=1:5
+	disp(learning_rate(z));
+	w = zeros(1,256);
+	for i = 1:1500
+		delta = zeros(1,256);
+		for j=1:1400
+			y1 = 1/(1+(exp(-(w)*(transpose(x(j,:))))));
+			err = y(j)-y1;
+			delta = delta+(err*x(j,:));
+		end
+		w = w+(learning_rate(z)*delta);
 	end
-	w = w+(learning_rate*delta);
+	disp(w);
 end
