c9144a95c5a4490535027b8505c7af26b794bfc3
Sr Kanna <kannas@flip2.engr.oregonstate.edu>
2017-04-21 12:21:53 -0700

    part4

--- hw2/hw2_4.m
+++ hw2/hw2_4.m
@@ -1,72 +1,54 @@
-accuracy = [];
+filename = 'usps-4-9-train.csv';
+[x,delimiterOut] = importdata(filename);
+y = x(:,end);
+x(:,end) = [];
+
+
+filename = 'usps-4-9-test.csv';
+[x2,delimiterOut] = importdata(filename);
+y2 = x2(:,end);
+x2(:,end) = [];
+
+
+accuracy_train = [];
+accuracy_test = [];
 c_train = 0;
+c_test = 0;
+total_train = 0
+total_test = 0;
 
 learning_rate = .0000001;
 w = zeros(1,256);
-total = 0;
+
 lambda = [.001,.01,.1,1,10,100];
 
 for z = 1:6
-	filename = 'usps-4-9-train.csv';
-	[x,delimiterOut] = importdata(filename);
-	y = x(:,end);
-	x(:,end) = [];
-
-
 	for i = 1:10000
 		delta = zeros(1,256);
 		for j=1:1400	
 
-			y1 = 1/(1+(exp(-(w)*(transpose(x(j,:))))));
-			zero = 1-y1;
-			ratio = y1/zero;
+			yhat = 1/(1+(exp(-(w)*(transpose(x(j,:))))));
+			zero = 1-yhat;
+			ratio = yhat/zero;
 
 			if (ratio >1)
 				prediction = 1;
 			else
 				prediction = 0;
 			end	
-
+	
 			if (prediction == y(j))
 				c_train = c_train+1;
 			end
-			err = y(j)-y1;
+			err = y(j)-yhat;
 			delta = delta+(err*x(j,:));
-			total = total+1;
+			total_train = total_train+1;
 		end
-		w = w+(learning_rate*(delta*lambda(z)));
-		ratio = c_train/total;
-		accuracy(i) = ratio;
-		c_train = 0;
-		total = 0;
-		%get a new w - update and guess based on this
-	end
-
-	filename = 'traindata.xlsx';
-	xlswrite(filename,accuracy);
-
-
-
-%read in from .csv file
-	filename = 'usps-4-9-test.csv';
-	[x,delimiterOut] = importdata(filename);
-
-%last column of contains two classes (0 = 4, 1 = 9);
-	y = x(:,end);
-	x(:,end) = [];
-
-
-	accuracy = [];
-	c_test = 0;
-	total = 0;
-
-	for i = 1:10000
-		delta = zeros(1,256);
-		for j=1:800	
-
-			y1 = 1/(1+(exp(-(w)*(transpose(x(j,:))))));
-			zero = 1-y1;
-			ratio = y1/zero;
+		for j=1:800
+	
+			yhat = 1/(1+(exp(-(w)*(transpose(x2(j,:))))));
+			zero = 1-yhat;
+			ratio = yhat/zero;
 
 			if (ratio >1)
 				prediction = 1;
@@ -74,25 +56,63 @@ for z = 1:6
 				prediction = 0;
 			end	
 
-			if (prediction == y(j))
+			if (prediction == y2(j))
 				c_test = c_test+1;
 			end
-			err = y(j)-y1;
-			delta = delta+(err*x(j,:));
-			total = total+1;
+			total_test = total_test+1;
 		end
-		ratio = c_test/total;
-		accuracy(i) = ratio;
+
+		w = w+(learning_rate*(delta+lambda(z)));
+		ratio = c_train/total_train;
+		accuracy_train(i) = ratio;
+	
+		ratio = c_test/total_test;
+		accuracy_test(i) = ratio;
+
 		c_test = 0;
-		total = 0;
+		c_train = 0;
+		total_test = 0;
+		total_train = 0;
+		%get a new w - update and guess based on this
 	end
 
-	if z == 1
-		filename = 'testdata.xlsx';
-		xlswrite(filename,accuracy);
-	elseif z == 2
-	elseif z == 3
-	elseif z == 4
-	elseif z == 5
-	elseif z == 6
-end;
+
+	if z ==1
+		filename = 'lambda1_train.csv';
+		csvwrite(filename,accuracy_train);
+		filename = 'lambda1_train.csv';
+		csvwrite(filename,accuracy_test);
+
+
+	elseif z ==2
+		filename = 'lambda2_train.csv';
+		csvwrite(filename,accuracy_train);
+		filename = 'lambda2_train.csv';
+		csvwrite(filename,accuracy_test);
+
+	elseif z ==3
+		filename = 'lambda3_train.csv';
+		csvwrite(filename,accuracy_train);
+		filename = 'lambda3_train.csv';
+		csvwrite(filename,accuracy_test);
+
+
+	elseif z ==4
+		filename = 'lambda4_train.csv';
+		csvwrite(filename,accuracy_train);
+		filename = 'lambda4_train.csv';
+		csvwrite(filename,accuracy_test);
+
+	elseif z ==5
+		filename = 'lambda5_train.csv';
+		csvwrite(filename,accuracy_train);
+		filename = 'lambda5_train.csv';
+		csvwrite(filename,accuracy_test);
+
+	else
+		filename = 'lambda6_train.csv';
+		csvwrite(filename,accuracy_train);
+		filename = 'lambda6_train.csv';
+		csvwrite(filename,accuracy_test);
+	end
+end
