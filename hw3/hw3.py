347e2b6b48e6f3a5fa9781786b790e422c432faf
Preston Wipf <gullysquad02@gmail.com>
2017-04-25 21:12:50 -0700

    problem 1 done, problem 2 basically done

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,46 +1,85 @@
 import numpy as np
-import math, random, sys
-import csv
-from numpy import genfromtxt
+import pandas as pd
+import math, random, sys, csv, operator
 from math import exp
 
 
+
 def main():
-	print 'Getting Data';
-	Xtrain, Ytrain, Xtest, Ytest = getData()
-
-	print 'starting part 1.1';
-	i = 1;
-	for i in range(51):
-		training_error, cross_validation, testing_error = euclidean(Xtrain,Ytrain,Xtest,Ytest,K)
+	print('\nProblem 1')
+	run_dataset(k=7)
 
 
-def getData():
-	training = genfromtxt('knn_train.csv', delimiter=',')
-	testing = genfromtxt('knn_test.csv', delimiter=',')
+
+def run_dataset(k):
+	print("\nrun_dataset")
+	X_train, Y_train, X_test, Y_test = get_data()
+
+	# Run training data
+	total = 0
+	correct = 0
+	for i in range(len(X_test)):
+		correctness = nearest_neighbor(X_train, Y_train, X_test[i], Y_test[i], k)
+		total += 1
+		correct += correctness
+	print("\nTraining Accuracy: ", (correct/total), "%%\n")
+
+	# Run testing data
+	total = 0
+	correct = 0
+	for i in range(len(X_test)):
+		correctness = nearest_neighbor(X_test, Y_test, X_test[i], Y_test[i], k)
+		total += 1
+		correct += correctness
+	print("\nTesting Accuracy: ", (correct/total), "%%\n")
 
 
-	XTrain = np.array(training[:,1:30])
-	YTrain = np.array(training[:,0:1])
-	XTest = np.array(testing[:,1:30])
-	YTest = np.array(testing[:,0:1])
 
-	return XTrain, YTrain, XTest, YTest
+def get_data():
+	df_train = pd.read_csv('knn_train.csv', header=None)
+	X_train = df_train.as_matrix(columns=list(range(1,31)))
+	Y_train = df_train.as_matrix(columns=[0])
+
+	df_test = pd.read_csv('knn_test.csv', header=None)
+	X_test = df_test.as_matrix(columns=list(range(1,31)))
+	Y_test = df_test.as_matrix(columns=[0])
+
+	return X_train, Y_train, X_test, Y_test
 
 
-def part1(Xtrain,Ytrain,Xtest,Ytest,K):
-	i = 0
-	j = 0
-	for i in range(247):
-		#check the distance	
-		distance = euclidean(Xtrain,Ytrain,Xtest,Ytest);
-		for j in range(51):
-			smallest = min(distance);
-				
 
-def euclidean(Xtrain,Ytrain,Xtest,Ytest):
-	return distance
+def nearest_neighbor(X, Y, x_instance, y_actual, k):
+	distances = []
+	neighbors = []
+	votes = []
+	for i in range(len(X)):
+		distances.append((X[i], get_distance(X, x_instance), Y[i]))
+	distances.sort(key=operator.itemgetter(1))
+	for i in range(k):
+		# This has the actual nearest neighbors (maybe unneccessary)
+		neighbors.append(distances[i][0])
+		# This has the classification of those neighbors
+		votes.append(distances[i][2])
+	prediction, confidence = classify(votes)
+	if prediction == y_actual: return 1
+	else: return 0
+
+
+
+def get_distance(X, x_i):
+	return np.sqrt(np.sum([np.matmul(np.transpose(np.subtract(X[j], x_i)), np.subtract(X[j], x_i)) for j in range(len(X))]))
+
+
+
+def classify(votes):
+	total = pos = neg = 0
+	for vote in votes:
+		total += 1
+		if vote == 1: pos += 1
+		if vote == -1: neg += 1
+	if pos > neg: return 1, (pos/total)
+	if neg > pos: return -1, (neg/total)
+
 
 if __name__ == '__main__':
 	main()
-
