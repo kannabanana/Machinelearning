6302ad34ce67dc034e6c73025e58484a3cccd8ef
Sr Kanna <kannas@flip3.engr.oregonstate.edu>
2017-04-29 13:09:06 -0700

    knn

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -6,15 +6,6 @@ from math import exp
 
 
 def main():
-	print 'Getting Data';
-	Xtrain, Ytrain, Xtest, Ytest = getData()
-	length = len(Xtest);
-
-	print 'starting part 1.1';
-	part1(Xtrain,Ytrain,Xtest,Ytest,1);
-#	k = 1;
-#	for k in range(51):
-#		training_error, cross_validation, testing_error = part1(Xtrain,Ytrain,Xtest,Ytest,k)
 	print('\nProblem 1')
 	run_dataset(k=7)
 
--- hw3/part1.py
+++ hw3/part1.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 import numpy as np
 import math, random, sys
 import csv
@@ -11,8 +12,9 @@ def main():
 	length = len(Xtest);
 
 	print 'starting part 1.1';
-	distance = euclidean(Xtrain,Xtest);
-	print distance
+	testing_accuracy = euclidean(Xtrain,Xtest,Ytest,Ytrain);
+	
+
 
 def getData():
 	training = genfromtxt('knn_train.csv', delimiter=',')
@@ -25,18 +27,57 @@ def getData():
 	YTest = np.array(testing[:,0:1])
 
 	return XTrain, YTrain, XTest, YTest
+	
 
 
-
-#find euclidean distance
-def euclidean(Xtrain,Xtest):
+def euclidean(Xtrain,Xtest,Ytest,Ytrain):
+	distances = [];
 	distance = 0
+	accuracy_total = [];
 	x = 0
-	for x in range(31):
-		distance += (Xtrain[x]-Xtest[x]) **2
-	return np.sqrt(distance)
+	for x in range(284):
+		accuracy_total.append(0);
+
+
+	x = 0
+	j = 0
+	for k in range(1,51):
+		print "k is ",k
+		for j in range(284):			#loop through all xtest values
+			for x in range(284):		#loop through all xtrain values
+				for a in range(29):
+					distance += (Xtrain[x][a]-Xtest[j][a]) **2
+					distance = np.sqrt(distance)
+				temp = [distance,x];
+				distances.append(temp);
+			neighbors = sorted(distances,key=itemgetter(0))			#sort distances based on given xtest point
+			
+			ben = 0
+			mal = 0
+			answer = 0
+			for b in range(k):
+				if (Ytrain[j] == 1):
+					mal = mal+1
+				else:
+					ben = ben+1
+			if (mal > ben):
+				answer = 1
+			else:
+				answer = -1
+		
+			if (answer == Ytest[j]):
+				accuracy_total[j] = accuracy_total[j]+1;
+	
+
+	x = 0
+	for x in range(284):
+		accuracy_total[k] = accuracy_total[k]/284;
+
+
+	print accuracy_total
+	return accuracy_total
+
 
 
 if __name__ == '__main__':
 	main()
-
